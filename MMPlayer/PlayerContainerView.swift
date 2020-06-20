8dcf10fbb7fafa923d375963c6c4aacbbe8d7bcc
Mahsa Mohammadi <eng.mohammadi.mng@gmail.com>
2020-06-20 10:01:19 +0430

    Revert "Fix quality list update title color"

--- MMPlayer/PlayerContainerView.swift
+++ MMPlayer/PlayerContainerView.swift
@@ -75,9 +75,6 @@ class PlayerContainerView: UIView    {
     var cashedTime: CGFloat = 0.0
     var sliderChangedValue: CGFloat = 0.0
     
-    let vodLink = ["480p x264", "720p x265", "1080p x265","Adaptive"]
-    var selectedActionSheetIndex = 0
-    
     func initializeView() {
         
         playerContainer.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: playerLayerHeight)
@@ -564,7 +561,9 @@ class PlayerContainerView: UIView    {
     
     //MARK: adaptive action sheet handling
     @IBAction func adaptiveBtnPressed(_ sender: Any) {
-        initializeCustomActionSheetView(vodlinks:vodLink, selectedIndex: selectedActionSheetIndex)
+        
+        let vodLink = ["480p x264", "720p x265", "1080p x265","Adaptive"]
+        initializeCustomActionSheetView(vodlinks:vodLink )
     }
     
     // actionSheet selector initialize
@@ -621,7 +620,7 @@ class PlayerContainerView: UIView    {
     // action sheet implementation
     var parentView: CustomActionSheetContainer?
     var actionSheetContainer: UIView?
-    func initializeCustomActionSheetView(vodlinks: Array<String>, selectedIndex: Int){
+    func initializeCustomActionSheetView(vodlinks: Array<String>){
         var titleArray = Array<String>.init()
         var linkArray = Array<String>.init()
         
@@ -695,7 +694,7 @@ class PlayerContainerView: UIView    {
             customActionSheet?.actionButton.setTitle(title, for: UIControl.State.normal)
             customActionSheet?.actionButton.addTarget(self, action: #selector(actionSheetTapped), for: UIControl.Event.allEvents)
             customActionSheet?.actionButton.tintColor = .white
-            if i == selectedIndex {
+            if i == 0 {
                 customActionSheet?.actionButton.tintColor = .red
             }
         }
@@ -720,37 +719,13 @@ class PlayerContainerView: UIView    {
         guard parentView?.linkDic[key!] != nil else{
             return
         }
-        
-        self.updateQualityList(link: (self.parentView?.linkDic[key!])!)
-        self.addNewLink(tmpLink: (parentView?.linkDic[key!])!, isLandScape: isVideoLandScape! , buttonTitle: key!)
-        
         dismissCustomActionSheet()
-    }
-    
-    @objc func updateQualityList(link: String) {
-        if let linkIndex = vodLink.firstIndex(where: {$0 == link}) {
-            self.selectedActionSheetIndex = linkIndex
-            let link = vodLink[linkIndex]
-            actionSheetContainer?.subviews.forEach({ customActionSheet in
-                if let sheet = customActionSheet as? CustomActionSheet {
-                    DispatchQueue.main.async {
-                        if sheet.actionButton.titleLabel?.text == link {
-                            sheet.actionButton.tintColor = .red
-                        } else {
-                            sheet.actionButton.tintColor = .white
-                        }
-                    }
-                    self.setNeedsLayout()
-                }
-            })
-        }
         
-      
+        self.addNewLink(tmpLink: (parentView?.linkDic[key!])!, isLandScape: isVideoLandScape! , buttonTitle: key!)
     }
     
     @objc func dismissCustomActionSheet() {
         UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 5, options: [.beginFromCurrentState], animations: {
-            
             self.parentView?.removeFromSuperview()
             self.actionSheetContainer?.removeFromSuperview()
         })
